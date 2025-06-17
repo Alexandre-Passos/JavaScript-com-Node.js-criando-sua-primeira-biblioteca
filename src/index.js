40e53fb87abaeb1d1a8a93964461b3ce7c5b2398
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-17 06:18:43 -0300

    #

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-const fs = require('fs')
+export function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto)
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  return contagem;
+}
 
-const caminhoArquivo = process.argv
-const link = caminhoArquivo[2]
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split('\n');
+}
 
-fs.readFile(link, 'utf-8', (err, texto) => {
-    verificaPalavrasDuplicadas(texto)
-})
-
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
 
 function verificaPalavrasDuplicadas(texto) {
-    const listaPalavras = texto.split(' ');
-    const resultado = {};
-    // objeto[propriedade] = valor;
-    listaPalavras.forEach(palavra => {
-        resultado[palavra] = (resultado[palavra] || 0) + 1
-    })
-    console.log(resultado);
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
+  })
+  return resultado;
 }
